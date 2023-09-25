0618dcf74ca64bacd1ed45ade75c9e0c3170d915
TWang <wang.tw.ted@gmail.com>
2023-09-26 02:02:26 +0800

    about player move: can climb wall can change gravity didn't solved: have trouble when walking outside the rectangle

--- game6/code/player6_move.cs
+++ game6/code/player6_move.cs
@@ -7,52 +7,74 @@ public class player6_move : MonoBehaviour
     Color black = new Color(225, 225, 225, 225), white = new Color(0, 0, 0, 225);
     void Start()
     {
-        collider_detect_2.collid = false;
         gameObject.GetComponent<Rigidbody2D>() .gravityScale = 1;
     }
-
     // Update is called once per frame
     void Update()
     {
         if (collider_detect_2.collid) {
             gameObject.GetComponent<Rigidbody2D>() .gravityScale = 0;
-            if (collider_detect_1.collid || collider_detect_0.collid) {
-                collider_detect_2.collid = false;
-            }
-        }
-        if (collider_detect_1.collid) {
+        } else if (collider_detect_3.collid) {
+            gameObject.GetComponent<Rigidbody2D>() .gravityScale = 0;
+        } else if (collider_detect_0.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
+            gameObject.GetComponent<Rigidbody2D>() .gravityScale = 1;
+        } else if (collider_detect_1.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
             gameObject.GetComponent<Rigidbody2D>() .gravityScale = -1;
-            // if (collider_detect_2.collid) {
-            //     collider_detect_1.collid = false;
-            // }
-        }
-        if (collider_detect_0.collid) {
-            if (collider_detect_2.collid) {
-                gameObject.GetComponent<Rigidbody2D>() .gravityScale = 0;
-                gameObject.transform.position += new Vector3(0, 0.5f, 0);
-                collider_detect_0.collid = false;
-            }
+        } else {
             gameObject.GetComponent<Rigidbody2D>() .gravityScale = 1;
         }
 
-        if (Input.GetKey("a") && collider_detect_0.collid) {
-            transform.Translate(-12*Time.deltaTime, 0, 0);
-            collider_detect_0.collid = false;
-        } else if (Input.GetKey("a") && collider_detect_1.collid) {
-            transform.Translate(12*Time.deltaTime, 0, 0);
-            collider_detect_1.collid = false;
+        if (Input.GetKey("a")) {
+            if (collider_detect_0.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
+                transform.Translate(-12*Time.deltaTime, 0, 0);
+                if (!collider_detect_0.collid) {
+                    gameObject.transform.position += new Vector3(0, -0.1f, 0);
+                }
+            } else if (collider_detect_1.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
+                transform.Translate(-12*Time.deltaTime, 0, 0);
+                if (!collider_detect_1.collid) {
+                    gameObject.transform.position += new Vector3(0, 0.1f, 0);
+                }
+            } else if ((collider_detect_0.collid && !collider_detect_1.collid) && (collider_detect_2.collid || collider_detect_3.collid)) {
+                gameObject.transform.position += new Vector3(0, 0.1f, 0);
+            } else if ((!collider_detect_0.collid && collider_detect_1.collid) && (collider_detect_2.collid || collider_detect_3.collid)) {
+                gameObject.transform.position += new Vector3(0, -0.1f, 0);
+            }
         }
-        if (Input.GetKey("d") && collider_detect_0.collid) {
-            transform.Translate(12*Time.deltaTime, 0, 0);
-            collider_detect_0.collid = false;
-        } else if (Input.GetKey("d") && collider_detect_1.collid) {
-            transform.Translate(-12*Time.deltaTime, 0, 0);
-            collider_detect_1.collid = false;
+        if (Input.GetKey("d")) {
+            if (collider_detect_0.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
+                transform.Translate(12*Time.deltaTime, 0, 0);
+                if (!collider_detect_0.collid) {
+                    gameObject.transform.position += new Vector3(0, -0.1f, 0);
+                }
+            } else if (collider_detect_1.collid && !collider_detect_2.collid && !collider_detect_3.collid) {
+                transform.Translate(12*Time.deltaTime, 0, 0);
+                if (!collider_detect_1.collid) {
+                    gameObject.transform.position += new Vector3(0, 0.1f, 0);
+                }
+            } else if (collider_detect_0.collid && (collider_detect_2.collid || collider_detect_3.collid)) {
+                gameObject.transform.position += new Vector3(0, 0.1f, 0);
+            } else if (collider_detect_1.collid && (collider_detect_2.collid || collider_detect_3.collid)) {
+                gameObject.transform.position += new Vector3(0, -0.1f, 0);
+            }
         }
-        if (Input.GetKey("w") && collider_detect_2.collid) {
-            transform.Translate(0, 2*Time.deltaTime, 0);
-        } else if (Input.GetKey("s") && collider_detect_2.collid) {
-            transform.Translate(0, -2*Time.deltaTime, 0);
+        if (Input.GetKey("w")) {
+            if ((collider_detect_2.collid || collider_detect_3.collid) && !collider_detect_0.collid && !collider_detect_1.collid) {
+                transform.Translate(0, 12*Time.deltaTime, 0);
+            } else if ((collider_detect_2.collid && !collider_detect_3.collid) && (collider_detect_0.collid || collider_detect_1.collid)) {
+                gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+            } else if ((!collider_detect_2.collid && collider_detect_3.collid) && (collider_detect_0.collid || collider_detect_1.collid)) {
+                gameObject.transform.position += new Vector3(0.1f, 0, 0);
+            }
+        }
+        if (Input.GetKey("s")) {
+            if ((collider_detect_2.collid || collider_detect_3.collid) && !collider_detect_0.collid && !collider_detect_1.collid) {
+                transform.Translate(0, -12*Time.deltaTime, 0);
+            } else if ((collider_detect_2.collid && !collider_detect_3.collid) && (collider_detect_0.collid || collider_detect_1.collid)) {
+                gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+            } else if ((!collider_detect_2.collid && collider_detect_3.collid) && (collider_detect_0.collid || collider_detect_1.collid)) {
+                gameObject.transform.position += new Vector3(0.1f, 0, 0);
+            }
         }
 
         if (Input.GetKey(KeyCode.Space)) {
